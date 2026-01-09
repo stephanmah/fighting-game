5bfbc66c5048c43807f26e6ee4ac48ef74c5deee
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2026-01-09 12:55:30 -0500

    Refactor character movement to use velocity
    Replaced manual position updates with velocity-based movement using move_and_slide(). Set default speed for the player in the scene file for more consistent movement handling.

--- Scenes/Character/character.gd
+++ Scenes/Character/character.gd
@@ -5,11 +5,6 @@ extends CharacterBody2D
 @export var speed : float
 
 func _process(delta:float) -> void: 
-	if Input.is_action_pressed("ui_right"):
-		position += Vector2.RIGHT * delta * speed
-	if Input.is_action_pressed("ui_left"):
-		position += Vector2.LEFT * delta * speed
-	if Input.is_action_pressed("ui_up"):
-		position += Vector2.UP * delta * speed
-	if Input.is_action_pressed("ui_down"):
-		position += Vector2.DOWN * delta * speed
+	var direction  := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = direction * speed
+	move_and_slide()
