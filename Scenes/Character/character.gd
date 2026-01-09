e5d13b98a98aad6724ef660633dfa2dc5bea467e
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2026-01-09 12:32:44 -0500

    Add player character to world and input actions
    Added a player character instance to the world scene and implemented basic movement logic in character.gd. Defined new input actions 'attack' and 'jump' in project settings to support player controls.

--- Scenes/Character/character.gd
+++ Scenes/Character/character.gd
@@ -3,3 +3,7 @@ extends CharacterBody2D
 @export var health : int
 @export var damage : int
 @export var speed : float
+
+func _process(delta:float) -> void: 
+		position += Vector2.RIGHT
+		print(delta)
